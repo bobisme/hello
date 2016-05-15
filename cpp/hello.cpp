4d7d4cf85d2d28edd238c3651821beece5fb6fda
BoB <bob@steinbauerusa.com>
2016-05-15 17:06:03 -0400

    use unordered_map

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <unordered_map>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "cout is goofy!" << std::endl;
+    unordered_map<string, string> m;
+    m["hello"] = "hey. cout is goofy";
+    cout << m["hello"] << endl;
     return 0;
 }
